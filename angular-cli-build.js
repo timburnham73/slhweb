f3ca58177472a00a266679d85f26c6a470657522
tim <tim.burnham@invicara.com>
2016-07-28 13:02:54 -0400

    Bootstrap working

--- angular-cli-build.js
+++ angular-cli-build.js
@@ -21,7 +21,8 @@ module.exports = function(defaults) {
       'angularfire2/**/*.js',
       'firebase/*.js',
       '@angular2-material/**/*',
-      'ng2-material/**/*.*'
+      'ng2-material/**/*.*',
+      'bootstrap/dist/**/*.*'
     ]
   });
 };

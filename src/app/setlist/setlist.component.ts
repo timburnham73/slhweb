57ca96d38d63069ed85230c135ecb4587ac76748
tim <tim.burnham@invicara.com>
2016-08-30 10:17:10 -0400

    Adding setlists page and getting basic data

--- src/app/setlist/setlist.component.ts
+++ src/app/setlist/setlist.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {AngularFire, FirebaseListObservable} from "angularfire2";
+import {AuthService} from "../account/auth/auth-service";
+import {Observable} from "rxjs";
 
 @Component({
   moduleId: module.id,
@@ -7,8 +10,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['setlist.component.css']
 })
 export class SetlistComponent implements OnInit {
+  items: FirebaseListObservable<any[]>;
+  setlists: Observable<any[]>;
 
-  constructor() {}
+  constructor(public af: AngularFire, auth: AuthService) {
+    const path = `/setlists`;
+    this.items = af.database.list(path);
+    //console.log('lodash version:', _.VERSION);
+    this.setlists = af.database.list(path,{
+      query:{
+        orderByChild: 'name'
+      }
+    })
+      .map((setlists) => {
+        return setlists.filter(setlist => setlist.uid === auth.id).map((setlist) =>{
+          //setlist.songs = af.database.object(`/songs/${setlist.$key}`);
+          for(var i = 1; i <= setlist.songs.length; i++) {
+            console.log(setlist);
+          }
+          return setlist;
+        })
+      });
+  }
+
 
   ngOnInit() {
   }
